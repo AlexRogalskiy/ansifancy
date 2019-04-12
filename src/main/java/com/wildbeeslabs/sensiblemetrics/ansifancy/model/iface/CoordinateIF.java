b1dc472861fac57ed37fc80e8d5958c8d0b64c52
AlexanderRogalskiy <alexander.rogalskiy@paragon-software.com>
2019-04-12 15:42:39 +0300

    Changeset to fancy stream

---
+++ src/main/java/com/wildbeeslabs/sensiblemetrics/ansifancy/model/iface/CoordinateIF.java
@@ -0,0 +1,14 @@
+package com.wildbeeslabs.sensiblemetrics.ansifancy.model.iface;
+
+import java.io.Serializable;
+
+/**
+ * Coordinate interface declaration
+ *
+ * @author Alexander Rogalskiy
+ * @version 1.0
+ */
+public interface CoordinateIF extends Serializable {
+
+    int getValue();
+}
