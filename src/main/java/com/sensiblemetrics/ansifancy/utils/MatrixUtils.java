b0049810dca535ca4d626c70ac7ac5f28bcfbe73
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2019-07-31 06:11:10 +0300

    Changeset to project structure

--- src/main/java/com/sensiblemetrics/ansifancy/utils/MatrixUtils.java
+++ src/main/java/com/sensiblemetrics/ansifancy/utils/MatrixUtils.java
@@ -417,4 +417,33 @@ public class MatrixUtils {
     public static void checkBound(int index, int lowerBound, int upperBound) {
         ValidationUtils.isTrue(index >= lowerBound && index <= upperBound, String.format("Should be in range [{%s},{%s}]", lowerBound, upperBound));
     }
+
+    public Integer[][] transpose(final Integer[][] matrix) {
+        final Integer[][] result = new Integer[matrix[0].length][matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+        return result;
+    }
+
+    public Integer[][] sum(final Integer[][] matrixA, final Integer[][] matrixB) throws IllegalArgumentException {
+        if (!sameOrderMatrix(matrixA, matrixB)) {
+            throw new IllegalArgumentException("Matrix should have the same order.");
+        }
+        final Integer[][] result = new Integer[matrixA.length][matrixB.length];
+        for (int i = 0; i < matrixA.length; i++) {
+            for (int j = 0; j < matrixA.length; j++) {
+                result[i][j] = matrixA[i][j] + matrixB[i][j];
+            }
+        }
+        return result;
+    }
+
+    public boolean sameOrderMatrix(final Integer[][] matrixA, final Integer[][] matrixB) {
+        int counterLineA = matrixA.length, counterColumnA = matrixA[0].length;
+        int counterLineB = matrixB.length, counterColumnB = matrixB[0].length;
+        return counterLineA == counterLineB && counterColumnA == counterColumnB;
+    }
 }
